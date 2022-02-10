57f35a973137f4bcb6a5ce32404734531f5fbb31
Cristian Suzuki <cristiansuzuki5@hotmail.com>
2022-02-10 17:15:28 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Container } from "./styles";
+
+class Header extends Component {
+  render() {
+    return <Container>Hello W0rld !</Container>;
+  }
+}
+
+export default Header;
---
+++ src/styles/themes/light.js
@@ -0,0 +1,11 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#C62E65",
+    secundary: "#D63AF9",
+
+    background: "#FFF",
+    text: "#FFF",
+  },
+};
